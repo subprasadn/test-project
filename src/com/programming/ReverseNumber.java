a65740a2061f0e2678ced1e840421c9bf1957f46
Subrahmanya Prasad <subprasadn@gmail.com>
2017-08-20 13:24:46 +0530

    first

---
+++ src/com/programming/ReverseNumber.java
@@ -0,0 +1,20 @@
+package com.programming;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+  
+	double a=0%10;
+	double b=2%10;
+	float c=(float)5/10;
+	double d=0/10;
+	
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+
+	}
+
+}
+
